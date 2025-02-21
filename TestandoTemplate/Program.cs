d612eb4e73237bf264c301613727d8b6885d7cc0
Cadu <carlos.e.bueno@hotmail.com>
2025-02-21 17:01:29 -0300

    'primeiro'

---
+++ TestandoTemplate/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string password = "SuperSecretPassword123!";
+        Console.WriteLine("Hello, World!");
+    }
+}
